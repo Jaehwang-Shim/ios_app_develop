4a57dfde193bcc80f5c14773629fdc16090ee722
Jaehwang Shim <jaehwang19@gmail.com>
2024-07-23 19:18:42 +0900

    spatial touch for ios demo first

---
+++ middlework/Singleton/AppInfo.swift
@@ -0,0 +1,8 @@
+//
+//  AppInfo.swift
+//  Spatial_touch_demo2_for_ios
+//
+//  Created by Jaehwang on 7/23/24.
+//
+
+import Foundation
---
+++ middlework/controllers/InitViewController.swift
@@ -0,0 +1,29 @@
+//
+//  InitViewController.swift
+//  Spatial_touch_demo2_for_ios
+//
+//  Created by Jaehwang on 7/23/24.
+//
+
+import UIKit
+
+class InitViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
